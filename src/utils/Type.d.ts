9cc993bc38dd2a6bb4fa983b574fc735c0d3810c
huangjihua <steven.online@qq.com>
2022-08-11 20:05:16 +0800

    feat: add file enum type

---
+++ src/utils/Type.d.ts
@@ -0,0 +1,11 @@
+export enum FILE_ENUM_TYPE {
+  WOFF = 'application/font-woff',
+  TTF = 'application/font-truetype',
+  EOT = 'image/eotapplication/vnd.ms-fontobject',
+  GIF = 'image/gif',
+  TIFF = 'image/tiff',
+  SVG = 'image/svg+xml',
+  JPEG = 'image/jpeg',
+  JPG = 'image/jpeg',
+  PNG = 'image/png',
+}
