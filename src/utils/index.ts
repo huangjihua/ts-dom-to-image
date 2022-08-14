7f70f12df1b77591f2eeb275dfea95218946e679
hank <steven-online@hotmail.com>
2022-08-14 23:34:15 +0800

    fix:update utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,7 @@
 
 import { xhr } from './xhr';
-
+import * as util from './tool';
+const URL_REGEX = /url\(['"]?([^'"]+?)['"]?\)/g;
 /**
  * 图转成Base64编码
  * @param props :{
@@ -11,7 +12,7 @@ import { xhr } from './xhr';
   imagePlaceholder?: string  // base64
 }
  */
-export const ImgToBase64Encode = (props: {
+export const imgToBase64Encode = (props: {
   url: string,
   httpTimeout?: number,
   cacheBust?: boolean,
@@ -31,3 +32,144 @@ export const ImgToBase64Encode = (props: {
   })
 }
 
+/**
+ * 嵌入字体
+ * @param node 
+ * @returns 
+ */
+export const embedFonts = (node) => {
+  return newFontFaces().resolveAll().then(function (cssText) {
+    var styleNode = document.createElement('style');
+    node.appendChild(styleNode);
+    styleNode.appendChild(document.createTextNode(cssText));
+    return node;
+  });
+}
+
+
+function newFontFaces() {
+  return {
+    resolveAll: resolveAll,
+    impl: {
+      readAll: readAll,
+    },
+  };
+
+  function resolveAll() {
+    return readAll()
+      .then(function (webFonts) {
+        return Promise.all(
+          webFonts.map(webFont => webFont.resolve())
+        );
+      })
+      .then(function (cssStrings) {
+        return cssStrings.join('\n');
+      });
+  }
+
+  function readAll() {
+    return Promise.resolve(document.styleSheets)
+      .then(selectWebFontRules)
+      .then(function (rules) {
+        return rules.map(newWebFont);
+      });
+
+    function selectWebFontRules(styleSheets) {
+      const cssRules = styleSheets.cssRules
+      let result = [];
+      for (const rule of cssRules) {
+        if (rule.type === CSSRule.FONT_FACE_RULE) {
+          result.push(rule)
+        } else if (rule.style.getPropertyValue('src').search(URL_REGEX) !== -1) {
+          result.push(rule)
+        }
+      }
+      return result;
+    }
+  }
+
+  function newWebFont(webFontRule) {
+    return {
+      resolve: function resolve() {
+        var baseUrl = (webFontRule.parentStyleSheet || {}).href;
+        return newInliner().inlineAll(webFontRule.cssText, baseUrl);
+      },
+      src: function () {
+        return webFontRule.style.getPropertyValue('src');
+      },
+    };
+  }
+}
+}
+
+
+/**
+   * 外联资源转内联
+   */
+function newInliner() {
+
+  return {
+    inlineAll: inlineAll,
+    shouldProcess: shouldProcess,
+    impl: {
+      readUrls: readUrls,
+      inline: inline,
+    },
+  };
+
+  function shouldProcess(string) {
+    return string.search(URL_REGEX) !== -1;
+  }
+
+  function readUrls(string) {
+    var result = [];
+    var match;
+    while ((match = URL_REGEX.exec(string)) !== null) {
+      result.push(match[1]);
+    }
+    return result.filter(function (url) {
+      return !util.isDataUrl(url);
+    });
+  }
+
+  function inline(string, url, baseUrl, get) {
+    return Promise.resolve(url)
+      .then(function (url) {
+        return baseUrl ? util.createLinkUrl(url, baseUrl) : url;
+      })
+      .then(get || imgToBase64Encode)
+      .then(function (data) {
+        return util.dataAsBase64Url(data, util.ParsefileType(url));
+      })
+      .then(function (dataUrl) {
+        return string.replace(urlAsRegex(url), '$1' + dataUrl + '$3');
+      });
+
+    function urlAsRegex(url) {
+      return new RegExp(
+        '(url\\([\'"]?)(' + util.escape(url) + ')([\'"]?\\))',
+        'g'
+      );
+    }
+  }
+
+  function inlineAll(string, baseUrl, get?) {
+    if (nothingToInline()) return Promise.resolve(string);
+    console.log(string, baseUrl);
+    return Promise.resolve(string)
+      .then(readUrls)
+      .then(function (urls) {
+        var done = Promise.resolve(string);
+        urls.forEach(function (url) {
+          done = done.then(function (string) {
+            return inline(string, url, baseUrl, get);
+          });
+        });
+        return done;
+      });
+
+    function nothingToInline() {
+      return !shouldProcess(string);
+    }
+  }
+}
--- src/utils/tool.ts
+++ src/utils/tool.ts
@@ -102,6 +102,18 @@ export const createImage = (url) => {
   })
 }
 
+export const createLinkUrl = (url: string, baseUrl: string): string => {
+  var doc = document.implementation.createHTMLDocument();
+  var base = doc.createElement('base');
+  doc.head.appendChild(base);
+  var a = doc.createElement('a');
+  doc.body.appendChild(a);
+  base.href = baseUrl;
+  a.href = url;
+  console.log(url);
+  return a.href;
+}
+
 /**
  * 克隆元素样式
  * @param sourceNode 
@@ -122,7 +134,6 @@ const cloneStyle = (sourceNode, cloneNode) => {
     }
   }
 }
-
 /**
  * 
  * @param {HTMLElement} node 
@@ -133,116 +144,85 @@ const cloneStyle = (sourceNode, cloneNode) => {
 export const cloneNode = (node: HTMLElement, filter?: any, root = false) => {
   if (!root && filter && !filter(node)) return Promise.resolve();
   return Promise.resolve(node)
-    .then(makeNodeCopy)
-    .then(function (clone) {
-      return cloneChildren(node, clone, filter);
+    .then(node => {
+      return node instanceof HTMLCanvasElement ? createImage(node.toDataURL()) : node.cloneNode(false)
     })
-    .then(function (clone) {
-      return processClone(node, clone);
-    });
-
-  function makeNodeCopy(node) {
-    if (node instanceof HTMLCanvasElement)
-      return createImage(node.toDataURL());
-    return node.cloneNode(false);
-  }
-
-  function cloneChildren(original, clone, filter) {
-    var children = original.childNodes;
-    if (children.length === 0) return Promise.resolve(clone);
-
-    return cloneChildrenInOrder(clone, asArray(children), filter).then(
-      function () {
-        return clone;
+    .then((clone: any) => {
+      var children = node.childNodes; // 子节点
+      if (children.length === 0) return Promise.resolve(clone);
+      for (const child of children) {
+        cloneNode(child as HTMLElement, filter)
+          .then(childClone => {
+            if (childClone) clone.appendChild(childClone)
+          })
       }
-    );
+      return clone;
+    })
+    .then((clone: HTMLElement) => processClone(node, clone));
 
-    function cloneChildrenInOrder(parent, children, filter) {
-      var done = Promise.resolve();
-      children.forEach((child) => {
-        done = done
-          .then((): any => cloneNode(child, filter))
-          .then((childClone: any) => {
-            if (childClone) parent.appendChild(childClone);
-          });
-      });
-      return done;
-    }
-  }
 
   /**
-     * 克隆
-     * @param {HTMLElement} original  原对象
-     * @param {HTMLElement} clone  克隆对象
-     */
+   * 深度克隆
+   * @param {HTMLElement} original  原对象
+   * @param {HTMLElement} clone  克隆对象
+   */
   function processClone(original: HTMLElement, clone: HTMLElement) {
     if (!(clone instanceof Element)) return clone;
-
-    return Promise.resolve()
-      .then(() => cloneStyle(
-        window.getComputedStyle(original), clone.style))
-      .then(clonePseudoElements)
-      .then(copyUserInput)
-      .then(fixSvg)
-      .then(function () {
-        return clone;
-      });
-
-
+    // 克隆伪类元素，处理样式
     function clonePseudoElements() {
       [':before', ':after'].forEach(function (element) {
         clonePseudoElement(element);
       });
 
       function clonePseudoElement(element) {
-        var style = window.getComputedStyle(original, element);
-        var content = style.getPropertyValue('content');
-
+        const style = window.getComputedStyle(original, element);
+        const content = style.getPropertyValue('content');
         if (content === '' || content === 'none') return;
-
-        var className = uid();
+        const className = uid();
         clone.className = clone.className + ' ' + className;
-        var styleElement = document.createElement('style');
+        const styleElement = document.createElement('style');
         styleElement.appendChild(formatPseudoElementStyle(className, element, style));
         clone.appendChild(styleElement);
 
         function formatPseudoElementStyle(className, element, style) {
-          var selector = '.' + className + ':' + element;
-          var cssText = style.cssText ? formatCssText(style) : formatCssProperties(style);
-          return document.createTextNode(selector + '{' + cssText + '}');
-          function formatCssText(style) {
-            var content = style.getPropertyValue('content');
-            return style.cssText + ' content: ' + content + ';';
-          }
-          function formatCssProperties(style) {
-            return asArray(style)
-              .map(formatProperty)
-              .join('; ') + ';';
-            function formatProperty(name) {
-              return name + ': ' +
-                style.getPropertyValue(name) +
-                (style.getPropertyPriority(name) ? ' !important' : '');
+          const selector = '.' + className + ':' + element;
+          const content = style.getPropertyValue('content');
+          let cssText = '';
+          // 格式化样式
+          if (style.cssText) {
+            cssText = `${style.cssText} content: ${content};`
+          } else {
+            for (const key of style) {
+              cssText += `${key}:${style.getPropertyValue(key)}${style.getPropertyPriority(key) ? ' !important' : ''};`
             }
           }
+          return document.createTextNode(selector + '{' + cssText + '}');
         }
       }
     }
 
+    // 处理用户输入
     function copyUserInput() {
       if (original instanceof HTMLTextAreaElement) clone.innerHTML = original.value;
       if (original instanceof HTMLInputElement) clone.setAttribute("value", original.value);
     }
 
+    // 处理SVG情况
     function fixSvg() {
       if (!(clone instanceof SVGElement)) return;
       clone.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
       if (!(clone instanceof SVGRectElement)) return;
-      ['width', 'height'].forEach(function (attribute) {
+      ['width', 'height'].forEach((attribute) => {
         var value = clone.getAttribute(attribute);
         if (!value) return;
-
         clone.style.setProperty(attribute, value);
       });
     }
+    return Promise.resolve()
+      .then(() => cloneStyle(window.getComputedStyle(original), clone.style))
+      .then(clonePseudoElements)
+      .then(copyUserInput)
+      .then(fixSvg)
+      .then(() => clone)
   }
 }
