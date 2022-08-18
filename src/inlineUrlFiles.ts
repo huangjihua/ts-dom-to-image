261d5ce925ec0bca14aa83c1ae7d8263f85e2d73
huangjihua <steven.online@qq.com>
2022-08-18 19:29:17 +0800

    fix: update inline  function

--- src/inlineUrlFiles.ts
+++ src/inlineUrlFiles.ts
@@ -1,15 +1,6 @@
 import * as util from './utils';
 import { imgToEncode, createLinkUrl } from "./operateImage";
 
-async function inline(str: string, url: string, baseUrl: string) {
-  url = baseUrl ? createLinkUrl(url, baseUrl) : url;
-  let result = ''
-  const imgData: string = await imgToEncode({ url: url })
-  const base64 = util.dataAsBase64Url(imgData, util.ParsefileType(url));
-  result = str.replace(util.urlAsRegex(url), '$1' + base64 + '$3');
-  return result;
-}
-
 /**
  * 处理所有资源文件
  * @param str 
@@ -21,6 +12,21 @@ export const inlineFileAll = (str: string, baseUrl?: any) => {
   console.log(str, baseUrl);
   const urls = util.readUrls(str);
   let done = Promise.resolve(str);
+  /**
+   * 
+   * @param str 
+   * @param url 
+   * @param baseUrl 
+   * @returns 
+   */
+  const inline = async (str: string, url: string, baseUrl: string) => {
+    url = baseUrl ? createLinkUrl(url, baseUrl) : url;
+    let result = ''
+    const imgData: string = await imgToEncode({ url: url })
+    const base64 = util.dataAsBase64Url(imgData, util.ParsefileType(url));
+    result = str.replace(util.urlAsRegex(url), '$1' + base64 + '$3');
+    return result;
+  }
   urls.forEach((url: string) => {
     done = done.then((str: string) => {
       return inline(str, url, baseUrl);
