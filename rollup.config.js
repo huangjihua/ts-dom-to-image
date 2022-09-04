cff438ba274be7123ba58d2bf573dffe713428e8
hank <steven-online@hotmail.com>
2022-09-05 00:19:35 +0800

    feat: 打包增加压缩和sourcemap

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,24 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import typescript from 'rollup-plugin-typescript';
+import { terser } from 'rollup-plugin-terser';
 import pkg from './package.json';
+
+const production = process.env.BUILD === 'true';
+
+const plugins = [
+  typescript(), //解析 typescirpt
+  resolve(), // 查找和打包 node_moudles 中的第三方模块
+  commonjs(), // 将 CommonJS 转换成 ES2015 模块供 Rollup 处理
+  production && terser({
+    // output: {
+    //   ascii_only: true, // 仅输出ascii字符
+    // },
+    compress: {
+      pure_funcs: ['console.log'], // 去掉console.log函数
+    },
+  }),
+];
 
 export default [
   // UMD for brower-friendly build
@@ -11,20 +28,24 @@ export default [
       name: 'dom-to-image', //  umd 必填
       file: pkg.brower, // 终打包出来的文件路径和文件名，这里是在package.json的browser: 'dist/index.js'字段中配置的
       format: 'umd', // umd是兼容amd/cjs/iife的通用打包格式，适合浏览器
+      sourcemap:true
     },
-    plugins: [
-      resolve(), // 查找和打包 node_moudles 中的第三方模块
-      commonjs(), // 将 CommonJS 转换成 ES2015 模块供 Rollup 处理
-      typescript(), //解析 typescirpt
-    ],
+    plugins: plugins,
   },
   {
     input: 'src/index.ts',
     external: ['ms'],
-    plugins: [typescript()],
+    plugins: [
+      typescript(),
+      terser({
+        compress: {
+          pure_funcs: ['console.log', 'alert'], // 去掉console.log函数
+        },
+      }),
+    ],
     output: [
-      { file: pkg.main, format: 'cjs', exports: 'auto' },
-      { file: pkg.module, format: 'es', exports: 'auto' },
+      { file: pkg.main, format: 'cjs', exports: 'auto', sourcemap: true },
+      { file: pkg.module, format: 'es', exports: 'auto', sourcemap: true },
     ],
   },
 ];
