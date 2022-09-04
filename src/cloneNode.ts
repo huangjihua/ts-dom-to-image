4a8df92d1b5b7dc4edc1dea3889cf1c2ad25c4aa
hank <steven-online@hotmail.com>
2022-09-04 22:51:10 +0800

    fix：增加表达元素和svg处理方法

--- src/cloneNode.ts
+++ src/cloneNode.ts
@@ -4,11 +4,11 @@ import { processNodePseudoStyle } from './process-style';
 /**
  * 克隆元素
  * @param {HTMLElement} node 
- * @param {object} filter 
+ * @param {Function} filter
  * @param {boolean} root 
  * @returns 
  */
-export const cloneNode = async (node: HTMLElement, filter?: any, root = false) => {
+export const cloneNode = async (node: HTMLElement, filter?: Function, root = false) => {
   if (!root && filter && !filter(node)) return;
   var children = node.childNodes;
   const clone: any = node instanceof HTMLCanvasElement ? await createImage(node.toDataURL()) : node.cloneNode(false)
@@ -23,4 +23,31 @@ export const cloneNode = async (node: HTMLElement, filter?: any, root = false) =
       })
   }
   return clone
+}
+
+/**
+ * 处理表单元素
+ * @param {HTMLElement} clone  克隆元素（目标对象）
+ * @param {HTMLElement} original  原元素
+ */
+export const formCloneElementValue = (clone: HTMLElement, original: HTMLElement) => {
+  if (original instanceof HTMLTextAreaElement) clone.innerHTML = original.value;
+  if (original instanceof HTMLInputElement) clone.setAttribute("value", original.value);
+}
+
+
+/**
+ * 处理SVG
+ * @param clone 
+ * @returns 
+ */
+export const fixSvg = (clone: HTMLElement) => {
+  if (!(clone instanceof SVGElement)) return;
+  clone.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+  if (!(clone instanceof SVGRectElement)) return;
+  ['width', 'height'].forEach((attribute) => {
+    var value = clone.getAttribute(attribute);
+    if (!value) return;
+    clone.style.setProperty(attribute, value);
+  });
 }
