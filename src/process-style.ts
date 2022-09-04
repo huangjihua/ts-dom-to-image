1b9e83ef2ee92aa2540aefc914a00c3ca8cbe281
hank <steven-online@hotmail.com>
2022-09-04 22:52:30 +0800

    fix： 优化样式处理以及外联背景

--- src/process-style.ts
+++ src/process-style.ts
@@ -1,5 +1,5 @@
-import { uid } from "./utils";
-
+import * as util from "./utils";
+import { formCloneElementValue,fixSvg} from './cloneNode'
 /**
  *  设置克隆元素样式
  * @param  {CSSStyleDeclaration} sourceNodeCssStyle
@@ -20,9 +20,31 @@ export const setCloneNodeStyleProperty = (sourceNodeCssStyle: CSSStyleDeclaratio
   }
 }
 
+
+/**
+ *  检测图片元素和样式内的背景图，并转换为内联的 Base64形式
+ * @param node HTMLELment
+ */
+export const checkElementImgToInline = async (node: HTMLElement) => {
+ 
+  if (node.style) {
+    const background = node.style.getPropertyValue('background');
+    if (!background) return node;
+    const value = await util.checkStrUrlFile(background);
+    if (value) node.style.setProperty('background', value, node.style.getPropertyPriority('background'));
+  }
+  const arr = Array.prototype.slice.call(node.childNodes).filter(child => child.nodeType === 1)
+  await Promise.all(
+    arr.map((child: HTMLElement) => {
+      return checkElementImgToInline(child);
+    })
+  )
+  return node;
+}
+
 /**
  * 处理元素伪类情况
- * @param {HTMLElement} original  用于获取计算样式的Element。
+ * @param {HTMLElement} original  原元素
  * @param {HTMLElement} clone  克隆元素（目标对象）
  */
 export const processNodePseudoStyle = (original: HTMLElement, clone: HTMLElement) => {
@@ -37,54 +59,52 @@ export const processNodePseudoStyle = (original: HTMLElement, clone: HTMLElement
     const style = window.getComputedStyle(original, pseudoName);
     const content = style.getPropertyValue('content');
     if (content === '' || content === 'none') return;
-    const className = uid();
+    const className = util.uid();
     clone.className = clone.className + ' ' + className;
     const styleElement = document.createElement('style');
     styleElement.appendChild(getFormatPseudoStyle(className, pseudoName, style));
     clone.appendChild(styleElement);
-
-    /**
-     *  获取格式化后的TextNode
-     * @param {string} className 样式名
-     * @param {string} pseudoName 伪类名称 
-     * @param {CSSStyleDeclaration} style 样式声明对象
-     * @returns {TextNode}
-     */
-    function getFormatPseudoStyle(className: string, pseudoName: string, style: CSSStyleDeclaration) {
-      const selector = `.${className}:${pseudoName}`;
-      const content = style.getPropertyValue('content');
-      let cssText = '';
-      if (style.cssText) {
-        cssText = `${style.cssText} content: ${content};`
-      } else {
-        for (const key of style) {
-          cssText += `${key}:${style.getPropertyValue(key)}${style.getPropertyPriority(key) ? ' !important' : ''};`
-        }
-      }
-      return document.createTextNode(`${selector}{${cssText}}`);
-    }
   }
-  // 处理表单元素
-  function formCloneElementValue() {
-    if (original instanceof HTMLTextAreaElement) clone.innerHTML = original.value;
-    if (original instanceof HTMLInputElement) clone.setAttribute("value", original.value);
-  }
-
-  // 处理SVG
-  function fixSvg() {
-    if (!(clone instanceof SVGElement)) return;
-    clone.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
-    if (!(clone instanceof SVGRectElement)) return;
-    ['width', 'height'].forEach((attribute) => {
-      var value = clone.getAttribute(attribute);
-      if (!value) return;
-      clone.style.setProperty(attribute, value);
-    });
-  }
+ 
   return Promise.resolve()
     .then(() => setCloneNodeStyleProperty(window.getComputedStyle(original), clone.style))
     .then(eachPseudoStyles)
-    .then(formCloneElementValue)
-    .then(fixSvg)
+    .then(()=>formCloneElementValue(clone,original))
+    .then(() => fixSvg(clone))
     .then(() => clone)
+}
+
+/**
+ * 处理原始背景外联资源
+ * @param node 
+ * @returns {void}
+ */
+export const processNodeBackground =async (node: HTMLElement) => {
+  if (node.style) {
+    const background = node.style.getPropertyValue('background');
+    if (!background) return node;
+    const value = await util.checkStrUrlFile(background);
+    if (value) node.style.setProperty('background', value, node.style.getPropertyPriority('background'));
+  }
+}
+
+/**
+ *  获取格式化后的TextNode
+ * @param {string} className 样式名
+ * @param {string} pseudoName 伪类名称 
+ * @param {CSSStyleDeclaration} style 样式声明对象
+ * @returns {TextNode}
+ */
+function getFormatPseudoStyle(className: string, pseudoName: string, style: CSSStyleDeclaration) {
+  const selector = `.${className}:${pseudoName}`;
+  const content = style.getPropertyValue('content');
+  let cssText = '';
+  if (style.cssText) {
+    cssText = `${style.cssText} content: ${content};`
+  } else {
+    for (const key of style) {
+      cssText += `${key}:${style.getPropertyValue(key)}${style.getPropertyPriority(key) ? ' !important' : ''};`
+    }
+  }
+  return document.createTextNode(`${selector}{${cssText}}`);
 }
