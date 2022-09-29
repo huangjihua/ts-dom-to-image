65f3624501dd73d9df49447b1d1cb7140dd0b1c1
hank <steven-online@hotmail.com>
2022-09-30 00:48:24 +0800

    chore: 更新jest配以及新增包

--- jest.config.ts
+++ jest.config.ts
@@ -89,7 +89,6 @@ export default {
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   moduleNameMapper: {},
-
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
 
@@ -157,10 +156,10 @@ export default {
   // testLocationInResults: false,
 
   // The glob patterns Jest uses to detect test files
-  // testMatch: [
-  //   "**/__tests__/**/*.[jt]s?(x)",
-  //   "**/?(*.)+(spec|test).[tj]s?(x)"
-  // ],
+  testMatch: [
+    "**/__tests__/**/*.[jt]s?(x)",
+    "**/?(*.)+(spec|test).[tj]s?(x)"
+  ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   // testPathIgnorePatterns: [
@@ -171,7 +170,7 @@ export default {
   // testRegex: [],
 
   // This option allows the use of a custom results processor
-  // testResultsProcessor: undefined,
+  testResultsProcessor: "jest-report",
 
   // This option allows use of a custom test runner
   // testRunner: "jest-circus/runner",
--- scripts/commit.js
+++ scripts/commit.js
@@ -4,4 +4,17 @@ const defaultOption = {
   base: process.cwd(),
   binary: 'git',
   maxConcurrentProcesses: 6,
+  trimmed: false,
 }
+
+const git = simpleGit(defaultOption)
+await git.init()
+const status = await git.status()
+console.log(status)
+// 当前不为master分支，才提交推送
+if (status.current !== 'master') {
+  console.log(status.current)
+}
+
+// staged.length>0 需要要 commit
+// not_added.length>0 => git add .
