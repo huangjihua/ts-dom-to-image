5a554effe043be88f4204219d210a14129b3b06b
hank <steven-online@hotmail.com>
2022-10-01 19:45:37 +0800

    ci: update cz and commitlint

--- .cz-config.js
+++ .cz-config.js
@@ -13,16 +13,29 @@ module.exports = {
     { value: 'chore', name: 'chore:    构建过程或辅助工具的变动' },
     { value: 'revert', name: 'revert:   回退' },
     { value: 'build', name: 'build:    打包' },
+    {
+      value: 'ci',
+      name: 'ci:  更改持续集成软件的配置文件和package中的scripts命令，例如scopes: Travis, Circle等',
+    },
+  ],
+  scopes: [
+    { name: 'module' },
+    { name: 'utils' },
+    { name: 'config' },
+    { name: 'build' },
+    { name: 'test' },
+    { name: 'demo' },
+    { name: 'custom' },
   ],
   // 消息步骤
   messages: {
     type: '请选择提交类型:',
-    // scope: '请输入文件修改范围(可选):',
+    scope: '请输入文件修改范围(可选):',
     // used if allowCustomScopes is true
     customScope: '请输入修改范围(可选):',
     subject: '请简要描述提交(必填):',
     body: '请输入详细描述(可选，待优化去除，跳过即可):',
-    // breaking: 'List any BREAKING CHANGES (optional):\n',
+    breaking: '非兼容性说明 (可选):\n',
     footer: '请输入要关闭的issue(待优化去除，跳过即可):',
     confirmCommit: '确认使用以上信息提交？(y/n/e/h)',
   },
