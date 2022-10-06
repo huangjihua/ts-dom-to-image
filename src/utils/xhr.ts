b8a5d5ce3d5ab35cde54d567418f7cd68330f799
黄继华 <steven.online@qq.com>
2022-10-06 22:14:17 +0800

    refactor(utils): update xhr

--- src/utils/xhr.ts
+++ src/utils/xhr.ts
@@ -8,48 +8,48 @@ export const xhr = (props: {
   httpTimeout?: number
   cacheBust?: boolean // 是否绕过缓存
   useCredentials?: boolean // 是否跨域
-  successHandle?: Function
-  failHandle?: Function
+  successHandle?: Function | undefined
+  failHandle?: Function | undefined
 }) => {
   let { url } = props
   const {
     httpTimeout = 30000,
     cacheBust = false,
     useCredentials = false,
-    successHandle = () => '',
-    failHandle = () => 'fetch fail',
+    successHandle,
+    failHandle,
   } = props
   if (cacheBust) {
     // Cache bypass so we dont have CORS issues with cached images
     // Source: https://developer.mozilla.org/en/docs/Web/API/XMLHttpRequest/Using_XMLHttpRequest#Bypassing_the_cache
     url += (/\?/.test(url) ? '&' : '?') + new Date().getTime()
   }
-  return new Promise(function (resolve) {
+  return new Promise(function (resolve, reject) {
     const request = new XMLHttpRequest()
     request.onreadystatechange = handle
-    request.ontimeout = () =>
-      fail(
-        `timeout of ${httpTimeout}ms occured while fetching resource: ${url}`,
-      )
+    request.ontimeout = () => reject(`timeout of ${httpTimeout}ms occured while fetching resource: ${url}`)
+
     request.responseType = 'blob'
     request.timeout = httpTimeout
     if (useCredentials) request.withCredentials = true // 如果服务端设置了"Access-Control-Allow-Origin": "*"，客户端请求时无需再设置withCredentials: true
     request.open('GET', url, true)
     request.send()
-
+    request.onerror = reject;
     function handle() {
       if (request.readyState !== 4) return
       if (request.status === 200) {
-        successHandle(request, resolve)
+        if (successHandle instanceof Function) {
+          successHandle(request, resolve)
+        } else {
+          resolve(request)
+        }
       } else {
-        failHandle()
-        fail('cannot fetch resource: ' + url + ', status: ' + request.status)
-        return
+        if (failHandle instanceof Function) {
+          failHandle(request, reject)
+        } else {
+          reject(`cannot fetch resource: ${url}, status: ${request.status}`)
+        }
       }
-    }
-    function fail(message: string) {
-      console.error(message)
-      resolve('')
     }
   })
 }
