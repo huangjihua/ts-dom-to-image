42c7d67e847652dc274951d650c98d4de9e1852e
黄继华 <steven.online@qq.com>
2022-10-07 19:52:13 +0800

    ci: update config

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,27 +1,92 @@
 const shelljs = require('shelljs')
+const inquirer = require('inquirer')
+const simpleGit = require('simple-git')
 const chalk = require('chalk') // 美化控制台输出
-const checkBranch = require('./check')
+const ora = require('ora') // 6.1.2版本只支持ES Module
+const child_process = require('child_process')
 const versionFn = require('./publish.version')
 
-async function publish(newVer) {
+const defaultOption = {
+  base: process.cwd(),
+  binary: 'git',
+  maxConcurrentProcesses: 6,
+  trimmed: false,
+}
+const git = simpleGit(defaultOption)
+const spinner = ora()
+
+async function publish() {
+  // 初始化
+  await git.init()
   // 拉取最新版本
-  shelljs.exec('git pull')
+  await git.pull('origin', 'master')
   // 运行测试
-  shelljs.exec('npm run test:c') //通过yarn version更新版本号，但不自动添加git tag，而是在构建完成后由cli工具添加
-  shelljs.exec(`npm version ${newVer} --no-git-tag-version`)
-  await checkBranch() // 发布前确认是否为master分支，并检查是否有未提交文件
-  const newVersion = await versionFn()
-  console.log(newVersion)
+  // execSyncCmd('npm run test:c')
+  await checkCommit()
+  const { version: newVersion } = await versionFn()
+  //通过version更新版本号，但不自动添加git tag，而是在构建完成后由cli工具添加await checkBranch() // 发布前确认是否为master分支，并检查是否有未提交文件
+  execSyncCmd(`npm version ${newVersion} --no-git-tag-version`)
+
   // 提交发布代码
-  // shelljs.exec('git add . -A')
-  // shelljs.exec('yarn commit')
+  await git.add('./*')
+
+  // execSyncCmd('npm run commit')
+  await git.commit(`build: ${newVersion}`)
+  await git.tag(`-a v${newVersion} -m "build: ${newVersion}"`)
+  await git.push()
+  await git.push(['--tags'])
   // shelljs.exec(`git tag -a v${newVersion} -m "build: ${newVersion}"`)
   // shelljs.exec('git push')
 
   // shelljs.exec('git push --tags')
   // 构建
-  // shelljs.exec('npm run build')
-  //发布
-  // shelljs.exec('npm run publish --access=public')
+  shelljs.exec('npm run build')
+  // 发布
+  shelljs.exec('npm run publish --access=public')
+}
+/**
+ * 同步执行bash命令
+ *
+ * @param {*} cmd
+ */
+function execSyncCmd(cmd) {
+  const res = child_process.execSync(cmd, { stdio: 'inherit' })
+  console.log(res)
+}
+
+/**
+ *  发布前拉取并确认是否为master分支，并检查是否有未提交文件，询问是否提交文件
+ *
+ */
+async function checkCommit() {
+  // await git.init()
+  const status = await git.status()
+  // 拉取最新版本
+  await git.pull()
+  // 当前不为master分支，才提交推送
+  if (status.current !== 'master') {
+    spinner.fail(chalk`{red error:}  请在 master 分支上发布`)
+    shelljs.exit(1)
+  }
+  if (status.files.length > 0) {
+    spinner.fail(chalk`{red error：}{blue 有未提交文件！}`)
+    const { commitflag } = await inquirer.prompt([
+      {
+        type: 'list',
+        name: 'commitflag',
+        message: '是否提交所有未提交文件',
+        choices: [
+          { name: '提交', value: 1 },
+          { name: '不提交', value: 0 },
+          { name: '退出', value: 2 },
+        ],
+      },
+    ])
+    if (commitflag === 1) {
+      await git.add('.')
+    } else if (commitflag === 2) {
+      shelljs.exit(1)
+    }
+  }
 }
 publish()
--- scripts/publish.version.js
+++ scripts/publish.version.js
@@ -8,25 +8,26 @@ const newVersion = {
   minor: semver.inc(version, 'minor'),
   major: semver.inc(version, 'major'),
 }
-module.exports = inquirer.prompt([
-  {
-    type: 'list',
-    name: 'version',
-    message: '版本号更新方式：',
-    choices: [
-      { name: `使用当前版本(${version})`, value: version },
-      {
-        name: `v${newVersion.patch}:when you make backwards compatible bug fixes`,
-        value: newVersion.patch, //表示修订号,向后兼容的 bug 修正;
-      },
-      {
-        name: `v${newVersion.minor}: when you add functionality in a backwards compatible manner`,
-        value: newVersion.minor, // 表示次版本号，向后兼容的功能性改变；
-      },
-      {
-        name: `v${newVersion.major}: when you make incompatible API changes`,
-        value: newVersion.major, // 表示主版本号，非兼容性的重大API修改；
-      },
-    ],
-  },
-])
+module.exports = () =>
+  inquirer.prompt([
+    {
+      type: 'list',
+      name: 'version',
+      message: '版本号更新方式：',
+      choices: [
+        { name: `使用当前版本(${version})`, value: version },
+        {
+          name: `v${newVersion.patch}:when you make backwards compatible bug fixes`,
+          value: newVersion.patch, //表示修订号,向后兼容的 bug 修正;
+        },
+        {
+          name: `v${newVersion.minor}: when you add functionality in a backwards compatible manner`,
+          value: newVersion.minor, // 表示次版本号，向后兼容的功能性改变；
+        },
+        {
+          name: `v${newVersion.major}: when you make incompatible API changes`,
+          value: newVersion.major, // 表示主版本号，非兼容性的重大API修改；
+        },
+      ],
+    },
+  ])
